4291b08065ce73dbe57a1b89949fda98940713ef
AadarshBaral <adarsmain101@gmail.com>
2022-07-31 23:11:50 +0545

    fixed the problem: if you are on a certain tab for example about page then reload the page, you end up in the home page.

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ menu.addEventListener('click',function(){
     navigation.classList.toggle('hidden');
 })
 //Single page look
-const homePage = document.querySelector('.landing-flex-container');
 
 
 const aboutMeBtn = document.querySelector('.about');
 const homeBtn = document.querySelector('.home');
+const homePage = document.querySelector('.landing-flex-container');
 const abouMeContainer = document.querySelector('.about-page-container');
 const projectsContainer = document.querySelector('.projects-page-container');
-const projectBtn = document.querySelector('.projects');
 const contactsContainer = document.querySelector('.contacts-page-container');
+const projectBtn = document.querySelector('.projects');
 const contactBtn = document.querySelector('.contact');
 
 const typewriter = document.querySelector('.typewriter')
@@ -88,17 +88,6 @@ ide_tab_3.addEventListener('click',function(){
 
 
 
-
-
-aboutMeBtn.addEventListener('click',function(){
-    homePage.classList.add('hidden');
-
-    abouMeContainer.classList.remove('hidden');
-
-
-})
-
-
 ///Home page
 ///Home page
 ///Home page
@@ -108,12 +97,12 @@ homeBtn.addEventListener('click',function(){
     if (homePage.classList.contains('hidden')){
         homePage.classList.remove('hidden');
     }
+    localStorage.setItem('page','home')
+
     abouMeContainer.classList.add('hidden');
     contactsContainer.classList.add('hidden');
     projectsContainer.classList.add('hidden');
 })
-
-
 
 
 // projects page
@@ -121,17 +110,8 @@ projectBtn.addEventListener('click',function(){
     abouMeContainer.classList.add('hidden');
     projectsContainer.classList.remove('hidden');
     homePage.classList.add('hidden');
+    localStorage.setItem('page','project')
 })
-
-
-
-
-
-
-
-
-
-
 
 //Contact Page
 contactBtn.addEventListener('click',function(){
@@ -139,4 +119,53 @@ contactBtn.addEventListener('click',function(){
     projectsContainer.classList.add('hidden');
     contactsContainer.classList.remove('hidden');
     homePage.classList.add('hidden');
+    localStorage.setItem('page','contact')
+})
+
+// reload handeling
+//Success of this thing was one of the most fun thing i have ever experienced during my learning phases of javascript
+if (!(localStorage.getItem('page'))){
+    localStorage.setItem('page','home')
+}
+
+if (sessionStorage.getItem("is_reloaded")){
+    // getting the page info on reload. Here we especially look for the page session storage
+    const section = localStorage.getItem('page')
+    const test= document.querySelector(`[data-name="${section}"]`)
+    // then we are adding a class called active to that section which value is stored on the session
+    test.classList.add('active')
+
+
+    const main_pages_list =  document.querySelectorAll('.page-c');
+    const pageClass   = []
+    main_pages_list.forEach(function(elem){
+        pageClass.push(elem)
+    
+    })
+    // Here we check in pageClass if it has such element which contains 'active' class and if it has then set it to visible
+    // otherwise add hidden classes to all other page-containers. page-container includes home,contact,project,and about
+    pageClass.forEach(function(elem){
+        if (elem.classList.contains('active')){
+            if (elem.classList.contains('hidden')){
+                elem.classList.remove('hidden')
+            }
+        }else{
+            if (!(elem.classList.contains('hidden'))){
+                elem.classList.add('hidden')
+            }
+        }
+    })  
+}
+
+sessionStorage.setItem("is_reloaded", true);
+
+
+//page reload check ends here
+
+aboutMeBtn.addEventListener('click',function(){
+    homePage.classList.add('hidden');
+    localStorage.setItem('page','about')
+    abouMeContainer.classList.remove('hidden');
+
+
 })
